3674783336b51d88abe7093cf08806f1704cb68d
demirmehmet0 <demirmehmet0@yahoo.com>
2023-05-17 11:30:38 +0300

    refactor: add system workspace

--- src/migrations/m230523_200101_workspace_setting.php
+++ src/migrations/m230523_200101_workspace_setting.php
@@ -1,25 +1,35 @@
 <?php
 
+use portalium\workspace\models\Workspace;
 use yii\db\Migration;
 use portalium\site\Module;
 use portalium\site\models\Form;
+use portalium\workspace\models\WorkspaceUser;
 
-class m110523_200101_workspace_setting extends Migration
+class m230523_200101_workspace_setting extends Migration
 {
     public function up()
     {
 
+        $siteUserRole = Yii::$app->setting->getValue('site::user_role');
+        $siteAdminRole = Yii::$app->setting->getValue('site::admin_role');
         $this->insert(Module::$tablePrefix . 'setting', [
             'module' => 'workspace',
             'name' => 'workspace::available_roles',
             'label' => 'Available Roles',
             'type' => Form::TYPE_WIDGET,
+            'value' => $siteUserRole ? '{"storage":["' . $siteUserRole . ',' . $siteAdminRole . '"]}' : '',
             'config' => json_encode([
                 'widget' => '\portalium\workspace\widgets\AvailableRoles',
                 'options' => [
                 ]
             ])
         ]);
+        if ($siteUserRole) {
+            $workspace = new Workspace();
+            $workspace->name = 'SystemWorkspace';
+            $workspace->save();
+        }
         
     }
 
