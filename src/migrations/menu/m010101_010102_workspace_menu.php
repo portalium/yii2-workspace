c0d160c1b3f8d1c6def22d241e09b5966b3f0233
sudesuvar <sudesuvar51@gmail.com>
2024-07-03 11:45:47 +0000

    migration files updated

--- src/migrations/menu/m010101_010102_workspace_menu.php
+++ src/migrations/menu/m010101_010102_workspace_menu.php
@@ -34,7 +34,7 @@ class m010101_010102_workspace_menu extends Migration
         $id_item = MenuItem::find()->where(['slug' => 'site'])->one()->id_item;
         $this->batchInsert('menu_item', ['id_item', 'label', 'slug', 'type', 'style', 'data', 'sort', 'id_menu', 'name_auth', 'id_user', 'date_create', 'date_update'], [
             [NULL, 'Workspace', 'workspace-select', '2', '{"icon":"fa-building","color":"","iconSize":"","display":"1","childDisplay":"3"}', '{"data":{"module":"workspace","routeType":"widget","route":"portalium\\\\workspace\\\\widgets\\\\Workspace","model":"","menuRoute":null,"menuType":"web"}}', 10, $id_menu, 'user', 1,'2022-06-14 13:34:04', '2022-06-14 13:34:04'],
-            [NULL, 'Workspaces', 'workspace-workspaces', '2', '{"icon":"","color":"","iconSize":"","display":false,"childDisplay":false}', '{"data":{"module":"workspace","routeType":"action","route":"\\/workspace\\/default\\/index","model":null,"menuRoute":null,"menuType":"web"}}', '13', $id_menu, 'workspaceWebDefaultIndex', 1, '2022-06-13 15:32:26', '2022-06-13 15:32:26']
+            [NULL, 'Workspaces', 'workspace-workspaces', '2', '{"icon":"","color":"","iconSize":"","display":"","childDisplay":false}', '{"data":{"module":"workspace","routeType":"action","route":"\\/workspace\\/default\\/index","model":null,"menuRoute":null,"menuType":"web"}}', '13', $id_menu, 'workspaceWebDefaultIndex', 1, '2022-06-13 15:32:26', '2022-06-13 15:32:26']
         ]);
 
 
@@ -42,14 +42,6 @@ class m010101_010102_workspace_menu extends Migration
             'id_item' => $id_item,
             'id_child' => MenuItem::find()->where(['slug' => 'workspace-workspaces'])->one()->id_item,
         ]);
-
-        $id_menu_side = Menu::find()->where(['slug' => 'web-side-menu'])->one()->id_menu;
-
-        if ($id_menu_side) {
-            $this->batchInsert('menu_item', ['id_item', 'label', 'slug', 'type', 'style', 'data', 'sort', 'id_menu', 'name_auth', 'id_user', 'date_create', 'date_update'], [
-                [NULL, 'Workspaces', 'workspaces', '2', '{"icon":"fa-building","color":"","iconSize":"","display":"3","childDisplay":"","placement":"1"}', '{"data":{"module":"workspace","routeType":"action","route":"\\/workspace\\/default\\/index","model":"","menuRoute":null,"menuType":null}}', 2, $id_menu_side, 'user', 1, '2023-07-17 20:15:56', '2024-02-08 11:14:43'],
-            ]);
-        }
 
     }
 
